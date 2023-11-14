47a7e344669e5f0612efb8363b83ae0d6137ee15
Halil İbrahim <43646661+ibrahimozdmr@users.noreply.github.com>
2023-11-14 03:59:59 +0300

    out, 1.c, .gitignore out klasörü oluşturularak derlenen dosyalar bir klasörde toplandı. ilk dosya oluşturuldu ve 'merhaba dünya' yazısı ingilizce olarak yazıldı.  son olarak takip edilecek ve edilmeyecek dosyalar tanımlandı.

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Standart input-output functions (Standart giriş-çıkış fonksiyonları)
+#include <stdlib.h> // General utilities (Genel amaçlı fonksiyonlar diyebiliriz)
+// !!! include ile başlayan satırlar "header dosyaları" denmekte. Derleme işleminin erken aşamlarında çalışan komutlardır.
+
+int main()
+{
+    printf("Hello World!"); // printf fonksiyonu stdio sayesinde kullanmaktayız. bu ekrana bir şeyler yazmaya yarar.
+}
